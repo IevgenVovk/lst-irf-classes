de93f76c9cf11830cb3aa96cbdd8b01372ff3186
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2024-10-05 20:57:36 +0900

    Test example.

---
+++ src/iclass/markup/tests/markup.py
@@ -0,0 +1,61 @@
+import unittest
+import numpy as np
+import pandas as pd
+from unittest.mock import patch
+
+from iclass.markup import mkmarkup
+
+
+def get_ref_df(log_emin: float, log_emax: float, ebinsdec: int, nclasses: int, nsamples: int) -> pd.DataFrame:
+    _offset = np.arange(nclasses)
+    _loge = np.linspace(log_emin, log_emax, num=ebinsdec+1)[:-1]
+    _sample_id = np.arange(nsamples)
+
+    offset, loge, _ = np.meshgrid(_offset, _loge, _sample_id)
+    phi = np.random.uniform(0, 2*np.pi, size=offset.shape)
+    
+    offset = offset.flatten()
+    phi = phi.flatten()
+    loge = loge.flatten()
+
+    data = dict(
+        src_x = np.zeros(len(offset)),
+        src_y = np.zeros(len(offset)),
+        reco_src_x = offset * np.cos(phi),
+        reco_src_y = offset * np.sin(phi),
+        mc_energy = 10**loge,
+        psf_class_true = (offset + 1).astype(int)
+    )
+
+    return pd.DataFrame(data)
+
+
+class MarkupTest(unittest.TestCase):
+    @patch('pandas.read_hdf')
+    def test_mkmarkup(self, mock_read_hdf):
+        ebinsdec = 4
+
+        ref = get_ref_df(
+            log_emin = 0,
+            log_emax = 2,
+            ebinsdec = ebinsdec,
+            nclasses = 4,
+            nsamples = 100
+        )
+
+        mock_read_hdf.configure_mock(
+            return_value = ref
+        )
+        result = mkmarkup(
+            input_fname = 'dummy_input',
+            key = 'dummy_key',
+            ebinsdec = ebinsdec,
+            cuts = ''
+        )
+
+        self.assertTrue(
+            np.allclose(
+                result['psf_class'].values,
+                result['psf_class_true'].values
+            )
+        )
